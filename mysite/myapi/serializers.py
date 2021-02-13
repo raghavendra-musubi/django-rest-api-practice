48d5d341852ed9524cdecc4bdb16975533d488e4
raghavendra-musubi <raghavendra@techis.io>
2021-02-14 02:44:19 +0530

    basic API works

--- mysite/myapi/serializers.py
+++ mysite/myapi/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers 
 from .models import Hero
 
-class HeroSerializer(serializers.HyperlinkedModelSerializer):
+class HeroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hero
         fields = ('id','name','alias')
--- mysite/myapi/urls.py
+++ mysite/myapi/urls.py
@@ -3,9 +3,9 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('heroes',views.HeroViewSet)
+router.register('heroes', views.HeroViewSet)
 
 urlpatterns = [
-    path('',include(router.urls)),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls',namespace='rest_framework'))
 ]
